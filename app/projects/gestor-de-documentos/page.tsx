a25955387c7c681c1d370c89c84ba844da7ba3a3
Paulo Henrique <paulo.ferraz.campos@gmail.com>
2025-11-19 20:51:30 -0300

    Um sonho

---
+++ app/projects/gestor-de-documentos/page.tsx
@@ -0,0 +1,13 @@
+export default function ProjectViewPage() {
+  return (
+    <div className="p-8 max-w-3xl mx-auto text-zinc-200">
+      <h1 className="text-4xl font-bold mb-6 text-zinc-100">
+        Gestor de documentos
+      </h1>
+
+      <p className="text-zinc-400">
+        Aqui vai a descrição do projeto...
+      </p>
+    </div>
+  );
+}
