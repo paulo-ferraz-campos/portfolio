f455901634ba61ec3053f12b6c9913598866b947
Paulo Henrique <paulo.ferraz.campos@gmail.com>
2025-11-19 21:06:47 -0300

    Deploy versel error

--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -1,21 +1,25 @@
-import type { Project } from "@/.contentlayer/generated";
+import type { Project as CLProject } from "@/.contentlayer/generated";
 import Link from "next/link";
 
+type Project = CLProject & {
+  page: string;
+};
+
 type Props = {
-	project: Project;
+  project: Project;
 };
 
 export const Article: React.FC<Props> = ({ project }) => {
-	return (
-		<Link href={`/projects/${project.page}`}>
-			<article className="p-4 md:p-8">
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
-					{project.title}
-				</h2>
-				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
-					{project.description}
-				</p>
-			</article>
-		</Link>
-	);
+  return (
+    <Link href={`/projects/${project.page}`}>
+      <article className="p-4 md:p-8">
+        <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+          {project.title}
+        </h2>
+        <p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
+          {project.description}
+        </p>
+      </article>
+    </Link>
+  );
 };
